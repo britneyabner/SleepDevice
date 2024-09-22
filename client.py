88a1daa2fd3767484f662209e0a16912efe536a5
Britney <brodytabner@protonmail.com>
2024-09-22 14:38:47 -0500

    client publishes test message

--- client.py
+++ client.py
@@ -10,6 +10,7 @@ def run_client(broker, port, keepalive, topic):
     def on_connect(mqttc, userdata, flags, reason_code, properties):
         print(f"Connected with result code {reason_code}")
         mqttc.subscribe(topic)
+        client.publish("data", "test")
 
     def on_message(mqttc, userdata, msg):
         print(msg.topic + " " + str(msg.payload))
@@ -18,7 +19,6 @@ def run_client(broker, port, keepalive, topic):
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(broker, port, keepalive)
-    client.publish("data", "test")
     client.loop_forever()
 
 
