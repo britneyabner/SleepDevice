ecb295ec024a50405eac8d9e66c36f90c19d82d0
Britney <brodytabner@protonmail.com>
2024-09-22 15:04:51 -0500

    MqttClient and MqttServer classes

--- client.py
+++ client.py
@@ -6,21 +6,36 @@ KEEPALIVE = 60
 TOPIC = "data"
 
 
-def run_client(broker, port, keepalive, topic):
-    def on_connect(mqttc, userdata, flags, reason_code, properties):
-        print(f"Connected with result code {reason_code}")
-        mqttc.subscribe(topic)
-        client.publish("data", "test")
+def _on_connect(mqttc, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
 
-    def on_message(mqttc, userdata, msg):
-        print(msg.topic + " " + str(msg.payload))
 
-    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(broker, port, keepalive)
-    client.loop_forever()
+def _on_message(mqttc, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+
+
+class MqttClient:
+    def __init__(self, broker: str, port: int, keepalive: int, topic: str):
+        self.broker = broker
+        self.port = port
+        self.keepalive = keepalive
+        self.topic = topic
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        self.client.on_connect = _on_connect
+        self.client.on_message = _on_message
+
+    def connect(self):
+        self.client.connect(self.broker, self.port, self.keepalive)
+        self.client.subscribe(self.topic)
+
+    def publish(self, message: str):
+        self.client.publish(self.topic, message)
 
 
 if __name__ == "__main__":
-    run_client(BROKER, PORT, KEEPALIVE, TOPIC)
+    client = MqttClient(BROKER, PORT, KEEPALIVE, TOPIC)
+    client.connect()
+    client.client.loop_start()
+    while True:
+        client.publish("test")
+    client.client.loop_stop()
