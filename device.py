07186e981b2e3de9813d8f028fdb14018b52ffda
Britney <brodytabner@protonmail.com>
2024-09-25 14:27:00 -0500

    testing camera

--- device.py
+++ device.py
@@ -1,14 +1,19 @@
 import pyaudio
+import time
 import picamera2
 import libcamera
 
 
 def record():
     camera = picamera2.Picamera2()
-    camera.start_preview(picamera2.Preview.QTGL, x=100,
-                         y=200, width=800, height=600)
-    transform = libcamera.Transform(hflip=1)
-    camera.start()
+    video_config = camera.create_video_configuration()
+    camera.configure(video_config)
+    encoder = picamera2.encoders.H264Encoder(bitrate=10000000)
+    output = "test.h264"
+
+    camera.start_recording(encoder, output)
+    time.sleep(10)
+    camera.stop_recording()
 
 
 if __name__ == "__main__":
