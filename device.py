dd07d9773a072791a66f8f96c4db0f0990c3618a
Britney <brodytabner@protonmail.com>
2024-09-25 14:19:12 -0500

    testing camera

--- device.py
+++ device.py
@@ -1,11 +1,13 @@
 import pyaudio
 import picamera2
+import libcamera
 
 
 def record():
     camera = picamera2.Picamera2()
     camera.start_preview(picamera2.Preview.QTGL, x=100,
                          y=200, width=800, height=600)
+    transform = libcamera.Transform(hflip=1)
     camera.start()
 
 
