e623ed8225b70396e9aea27330ae767551ac0bd1
Britney <brodytabner@protonmail.com>
2024-12-11 19:21:59 -0600

    testing

--- microphone.py
+++ microphone.py
@@ -7,7 +7,7 @@ class Microphone:
         self.digital_pin = digital_pin
 
     def detect_sound(self) -> bool:
-        value = False
+        value = True
         try:
             with gpiod.request_lines(
                 self.device_path,
