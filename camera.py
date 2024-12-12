20e5afd43b58c823038ca3f3058bb0d710f55715
Britney <brodytabner@protonmail.com>
2024-12-11 20:02:18 -0600

    bug fix in camera.py

--- camera.py
+++ camera.py
@@ -35,7 +35,7 @@ class FrameExtracter:
     def capture_frame(self):
         ret, frame = self.vidcap.read()
         if ret:
-            cv2.imshow('Frame', frame)
+            return frame
 
 
 def test_record_video():
