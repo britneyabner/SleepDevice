54f67c58b128e84f2a18677c5bf2a82f38991be3
Britney <brodytabner@protonmail.com>
2024-12-11 19:38:02 -0600

    bug fix

--- microphone.py
+++ microphone.py
@@ -24,3 +24,24 @@ class Microphone:
                     print(value)
         finally:
             return value
+
+
+DEVICE_PATH = "/dev/gpiochip4"
+DIGIATL_PIN = 23
+
+
+def detect_audio(chip_path, line_offset):
+    with gpiod.request_lines(
+        chip_path,
+        consumer="detect_audio",
+        config={line_offset: gpiod.LineSettings(direction=Direction.INPUT)},
+    ) as request:
+        value = request.detect_audio(line_offset, value)
+        print(value)
+
+
+if __name__ == "__main__":
+    try:
+        detect_audio(DEVICE_PATH, DIGIATL_PIN)
+    finally:
+        pass
