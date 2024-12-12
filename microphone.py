64b5d0002a1bc190fd58d3edf378a948f22c6658
Britney <brodytabner@protonmail.com>
2024-12-11 19:42:00 -0600

    bug fix

--- microphone.py
+++ microphone.py
@@ -30,18 +30,18 @@ DEVICE_PATH = "/dev/gpiochip4"
 DIGIATL_PIN = 23
 
 
-def get_line_value(chip_path, line_offset):
+def _get_line_value(chip_path, line_offset):
     with gpiod.request_lines(
         chip_path,
         consumer="get-line-value",
         config={line_offset: gpiod.LineSettings(direction=Direction.INPUT)},
     ) as request:
-        value = request.detect_audio(line_offset, value)
+        value = request.get_value(line_offset, value)
         print(value)
 
 
 if __name__ == "__main__":
     try:
-        get_line_value(DEVICE_PATH, DIGIATL_PIN)
+        _get_line_value(DEVICE_PATH, DIGIATL_PIN)
     finally:
         pass
